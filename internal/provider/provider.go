79e3ddef25dadcc44a8fddeb835b6be504fed2b4
Nicolo Tafta <venditasetam@gmail.com>
2024-07-29 13:18:31 +0200

    control of clientid

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -81,7 +81,7 @@ func (p *firecrestProvider) Configure(ctx context.Context, req provider.Configur
 	client := NewFireCrestClient(baseURL, "")
 
 	var token string
-	if clientID != "" {
+	if clientID != "" && clientSecret != "" {
 		var err error
 		token, err = client.GetToken(clientID, clientSecret)
 		if err != nil {
